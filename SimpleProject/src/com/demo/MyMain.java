d32527ccf68dd1758a3c2001a180aac46d67a1f1
Chris Crenshaw <chris.crenshaw@8x8.com>
2013-01-06 10:43:21 -0500

    Added my Main class!

---
+++ SimpleProject/src/com/demo/MyMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author chris
+ *
+ */
+public class MyMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Running main!");
+	}
+
+}
